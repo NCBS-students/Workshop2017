7b8669c83d35edfe1eed21728e6107571b918da4
Dinesh Natesan <AbstractGeek@users.noreply.github.com>
2017-11-23 20:42:06 +0530

    Completed matlab codes

--- material/MATLAB/Day_1.m
+++ material/MATLAB/Day_1.m
@@ -49,3 +49,78 @@ switch a
     otherwise
         disp('a is not one or zero');
 end
+%% Find max of a list (solution - 1; code for algorithm in pdf)
+
+number_list = [841238 619645 209407 435618 832714 887185 164080 939827 179879 517613];
+max = 0
+
+for curr_num = number_list
+    if curr_num>max
+        max = curr_num;
+    end
+end
+
+fprintf('Current number is %d', max);
+%% A better max finding solution
+% The above method does not work for negative numbers. Below one will work for 
+% all real numbers.
+
+number_list = [-630 -260 -298 -538 -176 -596 -23 -693 -791 -271];
+max = number_list(1);
+
+for curr_num = number_list
+    if curr_num>max
+        max = curr_num;
+    end
+end
+
+fprintf('Current number is %d', max);
+%% Is the given number n a prime number?
+% Brute force way of solving it.
+
+number = 43.
+divisors = 2:number-1;
+% assume prime
+prime = true;
+
+for curr_divisor = divisors
+    if mod(number, curr_divisor) == 0
+        prime = false;
+        break;    
+    end
+end
+
+if prime
+    fprintf('%d is prime', number);
+else
+    fprintf('%d is not prime', number);
+end
+%% Find the first 100 prime numbers
+
+prime_numbers = [];        % Starting with an empty vector
+curr_number = 2;    % Starting number
+
+while(length(prime_numbers) <= 100)    % Keep running until number of primes is 100
+    % Copy the above code (careful with the variable names)
+    divisors = 2:curr_number-1;
+    
+    % assume prime
+    prime = true;
+    
+    for curr_divisor = divisors
+        if mod(curr_number, curr_divisor) == 0
+            prime = false;
+            break;
+        end
+    end
+    
+    if prime
+        prime_numbers = [prime_numbers, curr_number];
+    end
+    
+    curr_number = curr_number + 1;    
+end
+
+disp(prime_numbers);
+%% 
+% There are smarter ways of doing this. Try it out.
